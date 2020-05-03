ec951d6690302198e28d0721eb1bc1dd98cdd412
bcmbf14 <bcmbf14@gmail.com>
2020-05-04 05:01:55 +0900

    app key 생성

---
+++ BcbfForecast/BcbfForecast/Common.swift
@@ -0,0 +1,12 @@
+//
+//  Common.swift
+//  BcbfForecast
+//
+//  Created by jongchankim on 2020/05/04.
+//  Copyright © 2020 JongChan KIm. All rights reserved.
+//
+
+import Foundation
+
+
+let appKey = "l7xx3db99b1125e3421e9ad660651d5563b1"

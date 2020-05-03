1c3c0b9d64b6303be1e0fb611b008d48ea613387
bcmbf14 <bcmbf14@gmail.com>
2020-05-04 06:56:56 +0900

    셀 아울렛 연결

---
+++ BcbfForecast/BcbfForecast/Cell/ForecastTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  ForecastTableViewCell.swift
+//  BcbfForecast
+//
+//  Created by jongchankim on 2020/05/04.
+//  Copyright © 2020 JongChan KIm. All rights reserved.
+//
+
+import UIKit
+
+class ForecastTableViewCell: UITableViewCell {
+    
+    static let identifier = "ForecastTableViewCell"
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ BcbfForecast/BcbfForecast/Cell/SummaryTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  SummaryTableViewCell.swift
+//  BcbfForecast
+//
+//  Created by jongchankim on 2020/05/04.
+//  Copyright © 2020 JongChan KIm. All rights reserved.
+//
+
+import UIKit
+
+class SummaryTableViewCell: UITableViewCell {
+    
+    static let identifier = "SummaryTableViewCell"
+    
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBOutlet weak var minMaxLabel: UILabel!
+    
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
